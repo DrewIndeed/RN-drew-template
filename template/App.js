2123d075880d925294526aec560a974737762c43
Andrew Le <s3820098@rmit.edu.vn>
2022-06-17 11:21:13 +0700

    Adjust App.js + remove RN paper + adjust template repo

--- template/App.js
+++ template/App.js
@@ -1,62 +1,40 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Button, Title} from 'react-native-paper';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const ProfileScreen = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-      <Title>Welcome to the Profile Screen!</Title>
-      <Button
-        mode="contained"
-        color="blue"
-        onPress={() => navigation.navigate('Home')}>
-        Go to Home Screen
-      </Button>
-    </View>
-  );
-};
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-      <Title>Welcome to React Native Home Screen!</Title>
-      <Button
-        mode="contained"
-        color="blue"
-        onPress={() => navigation.navigate('Profile')}>
-        Go to Profile Screen
-      </Button>
-    </View>
-  );
-};
-
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
-};
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native - bootstrapped with Drew Template!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
-
-export default App;
